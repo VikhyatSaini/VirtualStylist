4aab09027d24d65a1883e1fba7b82b8800712d4e
VikhyatSaini <sainivikhyat045@gmail.com>
2026-05-26 11:55:42 +0530

    Implemented Terraform, Docker, and Kubernetes deployment

---
+++ terraform/main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "virtualstylist_sg" {
+  name = "virtualstylist_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "virtualstylist_server" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t3.small"
+
+  key_name               = "virtualstylist-key"
+
+  vpc_security_group_ids = [aws_security_group.virtualstylist_sg.id]
+
+  tags = {
+    Name = "VirtualStylist-DevOps"
+  }
+}
